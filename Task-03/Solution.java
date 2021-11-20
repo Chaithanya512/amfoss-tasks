6c40c5afad3b1f04ad7baed198dc59ac9aa775e8
Chaithanya kalyan <92280449+Chaithanya512@users.noreply.github.com>
2021-11-21 00:14:54 +0530

    Update Solution.java

--- Task-03/Solution.java
+++ Task-03/Solution.java
@@ -1,1 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         System.out.print("");
+        double n,i ;
+        double f =  0;
+        int k = (int) f;
+        Scanner in = new Scanner(System.in);
+        n = in.nextDouble();
+        for (i = 1; i <=n; i++) {
+            if (n%i == 0) {
+                k++;
+            }
+    }
+System.out.print(k);
+    }
+}
